821c71c085c4d60ba314d51c38eb5d6cde0fbf2d
Apansenko Roman <romis_97@mail.ru>
2020-10-14 20:57:14 +0600

    App Reversed is done

--- viceversa/views.py
+++ viceversa/views.py
@@ -11,7 +11,18 @@ def reverse(request):
 	while length>0:
 		text_rev += user_text[length-1]
 		length = length-1
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':text_rev})
+	
+	count = 0
+	flag = 0
+	for i in range(len(user_text)):
+	    if user_text[i] != ' ' and flag == 0:
+	        count += 1
+	        flag = 1
+	    else:
+	        if user_text[i] == ' ':
+	            flag = 0
+
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':text_rev, 'count_text':count})
 
 	#Так же можно было решить эту задачу селд образом
 	#text_rev = user_text[::-1] Этот вариант намного короче и проще
